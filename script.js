90b83f9af30aba67ca3ab0c08f67b2c1985ecce6
“davit <davitdvalashvili@gmail.com>
2023-01-21 18:15:48 +0400

    rpoject completed

--- script.js
+++ script.js
@@ -8,14 +8,16 @@ const total = document.querySelector(".total-amount").lastElementChild;
 const reset = document.querySelector("button");
 
 // reset all of input and output data
-
 reset.addEventListener("click", () => {
     numPeople.value = null;
     bill.value = null;
     customPercentage[0].value = null;
     tip.innerHTML = 0;
     total.innerHTML = 0;
+    document.querySelector("#text-box span:nth-of-type(2)").style.display = "none"
+    numPeople.style.outline= ""
 });
+
 
 // validate input information
 const allowCaracters = /^[0-9]*$/;
@@ -28,28 +30,50 @@ document.querySelectorAll("input").forEach(element =>
 )
 
 
+// calculates tip with "click" method
 document.querySelectorAll(".grid-box .percentage").forEach(element =>
     element.addEventListener("click", (event)=> {
-        let Target = event.target.textContent;
-        let numberOfPercentages = Number(Target.slice(0,-1));
-        if ( numPeople.value !== null || numPeople.value !== 0) {
-            tip.innerHTML = bill.value / 100 * Number(numberOfPercentages)/numPeople.value;
-            total.innerHTML = bill.value / 100 * Number(numberOfPercentages);
-        } else  {
+        const Target = event.target.textContent;
+        const numberOfPercentages = Number(Target.slice(0,-1));
+        const tipvalue1 = (bill.value /numPeople.value) / 100 * Number(numberOfPercentages)
+        if ( Number(numPeople.value) == 0 || Number(numPeople.value) == null ) {
             tip.innerHTML = 0;
             total.innerHTML = 0;
+
+        } else  {
+            tip.innerHTML = Math.round(10*tipvalue1)/10;
+            total.innerHTML = Math.round(10*tipvalue1 * numPeople.value)/10;
+            customPercentage[0].value = null;
         }
     }) 
 )
 
+// calculates tip with "input" method
 customPercentage[0].addEventListener("input", (event) => {
     let Target = customPercentage[0].value;
     let numberOfPercentages = Number(Target);
-    total.innerHTML = bill.value / 100 * Number(numberOfPercentages);
-    tip.innerHTML = bill.value / 100 * Number(numberOfPercentages)/numPeople.value;
+    let tipvalue2 = (bill.value /numPeople.value) / 100 * Number(numberOfPercentages)
+    if ( Number(numPeople.value) == 0 || Number(numPeople.value) == null ) {
+        tip.innerHTML = 0;
+        total.innerHTML = 0;
+    } else {
+        total.innerHTML = Math.round(10*tipvalue2 * numPeople.value)/10;
+        tip.innerHTML = Math.round(10*tipvalue2)/10;
+    }
+
 })
 
-
+// show error message
+numPeople.addEventListener("input", (event) => {
+    const AlertStr = document.querySelector("#text-box span:nth-of-type(2)");
+    if(numPeople.value==0 && numPeople.value.length > 0) {
+        AlertStr.style.display = "inline" 
+        numPeople.style.outline= "0.2rem solid #E17052"
+    } else {
+        AlertStr.style.display = "none"
+        numPeople.style.outline= ""
+    }
+})
 
 
 
